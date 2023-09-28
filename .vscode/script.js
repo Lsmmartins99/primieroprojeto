0d796e2c3db0a62f33d8a7b7f1bb64fb27cc1553
Lucas Martins <lucasmartins_lsm@live.com>
2023-09-28 11:16:07 -0300

    :tada: initial commit

---
+++ .vscode/script.js
@@ -0,0 +1,18 @@
+function toggleMode() {
+  const html = document.documentElement
+  html.classList.toggle("light")
+
+  //pegar a img
+  const img = document.querySelector("#profile img")
+  // substituir a imagem
+  if (html.classList.contains("light")) {
+    //se tiver light mode, adicionar a imagem
+    img.setAttribute("src", "./assets/avatar-light.png")
+  } else {
+    // se tiver sem light mode, marter a img normal
+    img.setAttribute("src", "./assets/avatar.png")
+  }
+
+
+} 
+
